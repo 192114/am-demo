3dc1a3c5c6055e20afd7bb389c5b74f36390de82
192114 <shadowSun192114@gmail.com>
2023-08-04 11:39:50 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { FloatingPanel } from 'antd-mobile'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="full-screen">
+      <FloatingPanel anchors={[200, 500]}>
+        11111
+      </FloatingPanel>
+    </div>
+  )
+}
+
+export default App
